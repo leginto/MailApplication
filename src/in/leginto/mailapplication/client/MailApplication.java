79f8a55089e8710ed63c544bba87d8bc0062ccfc
leginto <leginto.r@gmail.com>
2018-07-21 19:13:06 +0530

    created source

---
+++ src/in/leginto/mailapplication/client/MailApplication.java
@@ -0,0 +1,10 @@
+package in.leginto.mailapplication.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MailApplication implements EntryPoint {
+
+	public void onModuleLoad() {
+	
+	}
+}
